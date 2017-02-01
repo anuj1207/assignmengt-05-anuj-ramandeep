63c4a8a686ba29dc09b22daf4e94392eb1477c64
anuj1207 <anuj.saxena@knoldus.in>
2017-02-01 21:43:34 +0530

    Update ClientOpsTest.scala

--- src/test/scala/edu/knoldus/ClientOpsTest.scala
+++ src/test/scala/edu/knoldus/ClientOpsTest.scala
@@ -10,9 +10,17 @@ class ClientOpsTest extends FlatSpec{
   val clientOps = new ClientOps
 
   it should "return true in adding Client" in {
-    assert(clientOps.add(Client(101,"New","Sales",12)))
+    assert(clientOps.add(Client(101,5,"Raam","Tilak Nagar")))
+  }
+ 
+  it should "return true in updating a Client" in {
+    assert(clientOps.update(101,4,"Raamu","Subhash Nagar"))
   }
 
+  it should "return true in fetching Client" in {
+    assert(clientOps.fetch(101))
+  }
+ 
   it should "return true in deleting client" in {
     assert(clientOps.delete(101))
   }
